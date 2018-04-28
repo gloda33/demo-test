9fe2f3ba8869f41f6d0f67a74f21c63d54f98844
Piotr Głodowski <piotr.glodowski@warta.pl>
2018-04-28 20:18:46 +0200

    zmiany

--- src/main/java/eu/glodowski/aws/service/AwsService.java
+++ src/main/java/eu/glodowski/aws/service/AwsService.java
@@ -1,13 +1,14 @@
 package eu.glodowski.aws.service;
 
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.ec2.model.InstanceType;
+import eu.glodowski.aws.model.AwsAccessData;
 import eu.glodowski.aws.model.HostState;
 import eu.glodowski.aws.model.Instance;
 import lombok.RequiredArgsConstructor;
-//
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+//
 
 @Slf4j
 @Service
@@ -17,15 +18,15 @@ public class AwsService {
     public static void createInstance(Instance instance){
 
         String securityGroupName = "amazonSecurityGroup";
-        createNewSecurityGroup(
+        AwsAccessData credentials = CreateSecurityGroup.createSecurityGroup(
                 securityGroupName,
                 "Security group created from application",
-                instance.getRegion()
-        );
+                instance.getRegion());
+
 
         try {
             CreateInstance.sendRequest(instance.getImageId(), instance.getSpotPrice(), instance.getInstanceType(),
-                    instance.getInstanceCount(), instance.getRegion(), securityGroupName);
+                    instance.getInstanceCount(), instance.getRegion(), credentials.getSecurityGroupId());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -46,12 +47,4 @@ public class AwsService {
         System.out.println("Sukces");
     }
 
-    public static void createNewSecurityGroup(String groupName, String groupDecription, Regions region){
-        CreateSecurityGroup.createSecurityGroup(groupName, groupDecription, region);
-    }
-
-    public void createNewInstance (){
-
-
-    }
 }
